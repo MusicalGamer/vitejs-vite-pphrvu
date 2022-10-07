dfb8986cc18ff9f2b87f0348ece7b9e0aa3a5c3a
MusicalGamer <114187138+MusicalGamer@users.noreply.github.com>
2022-10-06 20:29:32 -0700

    I made stuff with text

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,16 @@
 <h1>Hello</h1>
+
+
+<p> 
+I used to have a dog and I really like that my friends have a dog as well.
+</p>
+
+<h2>This is the sound that the ducky makes quack quack quack</h2>
+
+<h4>I love this heading size</h4>
+<h1>No u dickhead</h1>
+<style>
+h1{
+  
+}
+</style>
