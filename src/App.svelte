08318d2b9d9caad75ebb034c70f3c3385d77843f
MusicalGamer <114187138+MusicalGamer@users.noreply.github.com>
2022-10-13 12:57:32 -0700

    made a lot of shit

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,19 @@
-<h1>Hello</h1> 
-<p>daddy zachary says hello and I really want to have a really small class with him because it makes my life more productive</p>
+<h1></h1>
+<h3>{age}</h3>
+
+<button on:click={onClick}>Increase</button>
+<button on:click={reduceTheAge}>Reduce</button>
+
 <style>
+h3
+{
+  background:blue;
+  color: white
+}
+h2
+{
+background:aqua;
+}
 h1
 {
   Background:red;
@@ -10,3 +23,15 @@ p
   Background: yellow;
 }
 </style>
+<script>
+let age;
+age=14
+
+function onClick() {
+age = age + 1
+}
+
+function reduceTheAge() {
+  age-=1
+}
+</script>
