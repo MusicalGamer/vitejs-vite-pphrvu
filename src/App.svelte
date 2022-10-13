86ce31088188f8d0600315f8d3ec0f8f3c4ee1ec
MusicalGamer <114187138+MusicalGamer@users.noreply.github.com>
2022-10-13 12:37:25 -0700

    made shit yellow and red

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,12 @@
-<h1>Hello</h1>
-
-
-<p> 
-I used to have a dog and I really like that my friends have a dog as well.
-</p>
-
-<h2>This is the sound that the ducky makes quack quack quack</h2>
-
-<h4>I love this heading size</h4>
-<h1>No u dickhead</h1>
+<h1>Hello</h1> 
+<p>daddy zachary says hello and I really want to have a really small class with him because it makes my life more productive</p>
 <style>
-h1{
-  
+h1
+{
+  Background:red;
+}
+p
+{
+  Background: yellow;
 }
 </style>
